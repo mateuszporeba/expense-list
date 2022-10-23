6b9f8440040e7683c90db12e8f93be06a90f3b2d
RubasznyChomik <poreba.mateusz@gmail.com>
2022-10-23 11:36:01 +0200

    xc

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import './ExpenseItem.css';
 
 export default function ExpenseItem() {
+
+    const expenseDate = new Date(2021, 9, 29);
+    const expenseTitle = 'Car insurance';
+    const expenseAmount = 294.67;
+
     return (
-        <div>
-            <div>Date</div>
-            <div><h2>Title</h2></div>
-            <div>Amount</div>
+        <div className='expense-item'>
+            <div>{expenseDate.toISOString()}</div>
+            <div className='expense-item__description'></div>
+            <div><h2>{expenseTitle}</h2></div>
+            <div className='expense-item__price'>${expenseAmount}</div>
         </div>
+        
     )
 }
