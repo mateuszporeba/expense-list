5bbbedbad690133683739838d1eb2d7ffe7883e8
RubasznyChomik <poreba.mateusz@gmail.com>
2022-11-16 14:57:54 +0100

    x2

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,33 +7,35 @@ import { v4 } from 'uuid'
 
 export default function Expenses(props) {
 
-    const [filteredYear, setFilteredYear] = useState('2020');
+    const [filteredYear, setFilteredYear] = useState('2022');
 
     const filterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear);
     };
-    
+
     const filteredExpenses = props.items.filter(expense => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
 
 
+    let expensesContent = <p>No expenses found.</p>;
+
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map((expense) => (
+            <ExpenseItem
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+                key={v4().toString()}
+            />
+        ));
+    }
+
     return (
         <div>
             <Card className='expenses'>
                 <ExpensesFilter selected={filteredYear} onFilterChange={filterChangeHandler} />
-                {filteredExpenses.map((expense) => (
-                    <ExpenseItem
-                        title={expense.title}
-                        amount={expense.amount}
-                        date={expense.date}
-                        key={v4().toString()}
-                    />
-                ))}
-
-
-
-
+                {expensesContent}
             </Card>
         </div>
     )
