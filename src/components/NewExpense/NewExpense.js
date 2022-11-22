3919e5c56c10775784e04ae797468a8ffb94d27a
RubasznyChomik <poreba.mateusz@gmail.com>
2022-11-22 10:59:00 +0100

    open form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,26 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ExpenseForm from './ExpenseForm'
 import './NewExpense.css'
 import { v4 } from 'uuid';
 
 export default function NewExpense(props) {
 
-const saveExpenseDataHandler = (enteredExpenseData) => {
-    const expenseData ={
-        ...enteredExpenseData,
-        id: v4().toString()
-        //id: Math.random().toString()
+    const [isEditing, setIsEditing] = useState(false);
+
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: v4().toString()
+            //id: Math.random().toString()
+        };
+        props.onAddExpense(expenseData);
+        setIsEditing(false);
     };
-    //console.log(expenseData.id);
-    // console.log('NewExpense expense Data: ' + expenseData.date);
-    // console.log('NewExpense expense Data: ' + expenseData.title);
-    // console.log('NewExpense expense Data: ' + expenseData[1]);
-    props.onAddExpense(expenseData);
-};
+
+    const startEditingHandler = () => {
+        setIsEditing(true);
+    };
+
+    const stopEditingHandler = () => {
+        setIsEditing(false);
+    }
 
     return (
         <div className='new-expense'>
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+            {!isEditing && (
+                <button onClick={startEditingHandler}>Add New Expense</button>
+            )}
+            {isEditing && (
+                <ExpenseForm
+                    onSaveExpenseData={saveExpenseDataHandler}
+                    onCancel={stopEditingHandler}
+                />
+            )}
         </div>
     )
 }
